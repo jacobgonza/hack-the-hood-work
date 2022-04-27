b0ed7170f6b5352ba774721afdc2d46ba4027f51
jacobgonza <103624878+jacobgonza@users.noreply.github.com>
2022-04-27 15:18:16 -0700

    Create fundamentals-practice.py

---
+++ fundamentals-practice.py
@@ -0,0 +1,20 @@
+from operator import truediv
+
+
+print("Hello world")
+name = "Henery"
+age = 25
+years_at_hth = 1.5
+playes_baskeball = True
+jersey_number = "13"
+
+car_name = "mitsubishi"
+x = 50
+
+print(name)
+print(age)
+print(years_at_hth)
+print(playes_baskeball)
+print(jersey_number)
+print(car_name)
+print(x)
